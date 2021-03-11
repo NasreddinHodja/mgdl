051eef2d1181f7c643395a5e9ac34a52f2e8d953
NasreddinHodja <tbizetde@gmail.com>
2021-03-11 20:12:39 -0300

    Downloader class

---
+++ downloader.py
@@ -0,0 +1,87 @@
+import os
+import shutil
+
+import zipfile
+
+class Downloader:
+
+    def __init__(self, manga_dir=None, manga_url=None):
+        self.manga_dir = manga_dir
+        self.manga_url = manga_url
+
+    def decompress(self):
+        os.chdir(self.manga_dir)
+
+        for vol_name in os.listdir():
+            if "vol" not in vol_name: continue
+
+            chap_num = "0" + vol_name.split("_")[1].split(".")[0]
+            chap_name = "chapter_" + chap_num
+
+            if chap_name not in os.listdir():
+                os.mkdir(chap_name)
+                shutil.copy(vol_name, f"{chap_name}/{vol_name}")
+
+            os.chdir(chap_name)
+
+            with zipfile.ZipFile(vol_name, "r") as zip_ref:
+                zip_ref.extractall(".")
+
+            os.remove("info.txt")
+
+            os.chdir("..")
+            os.remove(vol_name)
+
+        os.chdir("..")
+
+    def skip_chaps(self):
+        chaps = [c.split("_")[1]
+                for c in os.listdir(self.manga_dir) if "chapter" in c]
+        latest = max(chaps)
+
+        count = 0
+        for chap in chaps:
+            if chap <= latest: count += 1
+
+        return str(count)
+
+    def dir_to_url(self):
+        url_name = "-".join([w.capitalize() for w in self.manga_dir.split("_")])
+        self.manga_url = "https://manga4life.com/manga/" + url_name
+        return self.manga_url
+
+    def update(self):
+        self.dir_to_url()
+
+        download_cmd = ("manga-py -s " +
+                        self.skip_chaps() +
+                        " -R -d . " +
+                        self.manga_url)
+
+        os.system(download_cmd)
+
+        if self.manga_url.split("/")[-1] in os.listdir():
+            for vol in os.listdir(self.manga_url.split("/")[-1]):
+                if "vol" not in vol: continue
+
+                shutil.move(self.manga_url.split("/")[-1] + "/" + vol,
+                            self.manga_dir)
+
+            os.rmdir(self.manga_url.split("/")[-1])
+
+            decompress(self.manga_dir)
+
+    def download(self):
+        if self.manga_url is None:
+            raise Exception("No manga url")
+
+        download_cmd = "manga-py -R -d . " + self.manga_url
+        manga_name = self.manga_url.split("/")[-1]
+        self.manga_dir = manga_name.lower().replace("-", "_")
+
+        os.system(download_cmd)
+
+        os.listdir()
+        shutil.move(manga_name, self.manga_dir)
+
+        self.decompress()
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import shutil
 import zipfile
 
 from query import search
+from downloader import Downloader
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -24,51 +25,12 @@ def parse_args():
 
     return parser, args
 
-def decompress(manga_name):
-    os.chdir(manga_name)
-
-    for vol_name in os.listdir():
-        if "vol" not in vol_name: continue
-
-        chap_num = "0" + vol_name.split("_")[1].split(".")[0]
-        chap_name = "chapter_" + chap_num
-
-        if chap_name not in os.listdir():
-            os.mkdir(chap_name)
-            shutil.copy(vol_name, f"{chap_name}/{vol_name}")
-
-        os.chdir(chap_name)
-
-        with zipfile.ZipFile(vol_name, "r") as zip_ref:
-            zip_ref.extractall(".")
-
-        os.remove("info.txt")
-
-        os.chdir("..")
-        os.remove(vol_name)
-
-    os.chdir("..")
-
-def skip_chaps(manga):
-    chaps = [c.split("_")[1]
-             for c in os.listdir(manga) if "chapter" in c]
-    latest = max(chaps)
-
-    count = 0
-    for chap in chaps:
-        if chap <= latest: count += 1
-
-    return str(count)
-
-def name_to_url(name):
-    name = "-".join([w.capitalize() for w in name.split("_")])
-    return "https://manga4life.com/manga/" + name
-
 def main():
     os.chdir("/mnt/nasHDD/manga/")
 
     parser, args = parse_args()
 
+    dldr = Downloader()
 
     if args.search:
         manga_url = search()
@@ -78,32 +40,11 @@ def main():
         args.download = "https://manga4life.com/manga/" + manga_url
 
     if args.download is not None:
-        download_cmd = "manga-py -R -d . " + args.download
-        manga_name = args.download.split("/")[-1]
-
-        os.system(download_cmd)
-
-        shutil.move(manga_name, manga_name.lower().replace("-", "_"))
-        manga_name = manga_name.lower().replace("-", "_")
-
-        decompress(manga_name.lower())
-
+        dldr.manga_url = args.download
+        dldr.download()
     elif args.update is not None:
-        url = name_to_url(args.update)
-        download_cmd = "manga-py -s " + skip_chaps(args.update) + " -R -d . " + url
-
-        os.system(download_cmd)
-
-        if url.split("/")[-1] in os.listdir():
-            for vol in os.listdir(url.split("/")[-1]):
-                if "vol" not in vol: continue
-
-                shutil.move(url.split("/")[-1] + "/" + vol,
-                            args.update)
-
-            os.rmdir(url.split("/")[-1])
-
-            decompress(args.update)
+        dldr.manga_dir = args.update
+        dldr.update()
     else:
         print(parser.print_help())
 
